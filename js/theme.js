570eb9b3be4eced4fa0ba2bbffd14ca61843f86c
Jordan Chase <jchase@bostoninteractive.com>
2015-02-20 13:43:16 -0500

    Updating theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,4 +1,22 @@
 (function ($) {
+  // set the default bootstrap breakpoints and variables
+  var breakpoints = {
+    xs : 768,
+    sm : 992,
+    md : 1200
+  },
+  winWidth,
+  winHeight,
+  docWidth,
+  docHeight;
+  
+  function setVars(){ 
+    docHeight    = $(document).outerHeight(),
+    docWidth     = $(document).outerWidth(),
+    winHeight    = $(window).height(),
+    winWidth     = $(window).width();
+  }
+  
   /**
    * Swap out svg files for PNGs on unsupporting devices. Modrnizr determines
    * what an unsupporting device is by adding the .no-svg class to the html
@@ -11,24 +29,58 @@
       $(this).attr('src', src);
     });
   }
+  
+  /**
+   * Gives each bootstrap tab a unique URL.
+   */
+  var url = document.location.toString();
+  if (url.match('#')) {
+    $('.nav-tabs a[href=#'+url.split('#')[1]+']').tab('show') ;
+  }
+  $('.nav-tabs a').on('shown.bs.tab', function(e) {
+    window.location.hash = e.target.hash;
+  })
+
+  /**
+   * Stuff to run immediately upon page load
+   */
+  svgToPng();
+  setVars();
 
   /**
    * Stuff to run on resize.
    */
   $(window).resize(function() {
-    var width = $(window).width();
-    // Place functions to run here.
+    setVars();
+    waitForFinalEvent(function() {
+      
+      // Place functions here
+    
+    }, 500, "global");
   });
 
   /**
-   * Stuff to run on page load and ajax events.
+   * Stuff to run after page load is complete.
    */
-  Drupal.behaviors.bootstrapStarter = {
-    attach: function (context, settings) {
-      svgToPng();
-      // Place functions to run here.
-    }
-  };
+  $(document).ready(function() {
+    // Place functions here
+  });
+
+  /**
+   * Helper function to delay firing resize events until the user actually
+   * stops resizing their browser.
+   */
+  var waitForFinalEvent = (function () {
+    var timers = {};
+    return function (callback, ms, uniqueId) {
+      if (!uniqueId) {
+        uniqueId = "Don't call this twice without a uniqueId";
+      }
+      if (timers[uniqueId]) {
+        clearTimeout (timers[uniqueId]);
+      }
+      timers[uniqueId] = setTimeout(callback, ms);
+    };
+  })();
 
 }(jQuery));
-
